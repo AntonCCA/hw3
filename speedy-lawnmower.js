f6bcdacd66df8057dc417eba6affff28dca338a8
Anton Castro <47227338+AntonCCA@users.noreply.github.com>
2019-02-16 20:51:22 -0800

    Create speedy-lawnmower.js
    made it cut faster and taller after being cut

---
+++ speedy-lawnmower.js
@@ -0,0 +1,31 @@
+function setup() {
+  createCanvas(600, 600);
+  colorMode(HSB);
+}
+
+var x = 0;
+var h = 10;
+
+function draw() {
+  stroke(random(60, 70), 100, 90);
+  line(x, height-10, x+random(-10, 10), height-10-random(h));
+  noStroke();
+
+  x = x + 10;
+
+     if (x > width) {
+     x = random(10);
+     h = h + 10;
+  }
+
+      if (random() > 0.99) {
+      fill(255);
+      rect(0, 0, width, height-30);
+     h = 10;
+  }
+
+  fill(40, 100, 150);
+  rect(0, height-10, width, 10);
+  
+}
+
